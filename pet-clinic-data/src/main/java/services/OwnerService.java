835c46a24a7f0bd5dcf24af732ed0d478dd5f81c
SIVASPECTER <sivasankarbemech@gmail.com>
2023-01-31 03:36:08 +0530

    Closes #3 , Implement Map Service implemented

--- pet-clinic-data/src/main/java/services/OwnerService.java
+++ pet-clinic-data/src/main/java/services/OwnerService.java
@@ -1,9 +1,6 @@
 package services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.model.Pet;
-
-import java.util.Set;
 
 public interface OwnerService  extends CrudService<Owner,Long>{
 
